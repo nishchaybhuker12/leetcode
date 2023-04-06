4032ce05cca64808a69f058264113932ed67c50c
nishchaybhuker20 <nishchaybhuker123@gmail.com>
2023-04-06 20:39:16 +0530

    we use dfs to change the boundries into 1 as we mark them visited

---
+++ 06-04-23__Number_of_closed_Islands.cpp
@@ -0,0 +1,30 @@
+/*Given a 2D grid consists of 0s(land) and 1s (water). An island is a maximal 4-directionally connected group of 0s and a closed island is an island totally(all left, top, right, bottom) surrounded by 1s.
+*/
+
+class Solution{
+    int closedIsland(vector<vector<int>>& grid){
+     int rows = grid.size(), cols = grid[0].size(), count = 0;
+        
+        function<bool(int, int)> dfs = [&](int i, int j) {
+            if (i < 0  || j < 0  ||  i >= rows   || j >= cols) {
+                return false;
+            }
+            if (grid[i][j] == 1) {
+                return true;
+            }
+            grid[i][j] = 1; // mark as visited
+            bool left = dfs(i, j-1), right = dfs(i, j+1), up = dfs(i-1, j), down = dfs(i+1, j);
+            return left && right && up && down;
+        };
+        
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == 0 && dfs(i, j)) {
+                    count++;
+                }
+            }
+        }
+        
+        return count;
+    }
+};
