72e8b954a6df66d6267d92bf167eca230cb00f8a
nishchaybhuker20 <nishchaybhuker123@gmail.com>
2023-07-06 22:20:12 +0530

    using sliding window

---
+++ Minimum_Size_Subarray_Sum.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int i=0;
+        int j=0;
+        int sum=0;
+        int mn=INT_MAX;
+        while(j<nums.size()){
+            sum+=nums[j];
+            while(sum>=target){
+                sum-=nums[i];
+                mn=min(j-i+1,mn);
+                i++;
+            }
+            j++;
+        }
+        if(mn==INT_MAX){
+            return 0;
+        }
+        return mn;
+    }
+};
